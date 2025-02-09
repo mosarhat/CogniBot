669c560639fe4ea219284bcfc1c73a98512e3174
mosarhat <mosarhat.01@gmail.com>
2025-02-08 21:13:53 -0500

    update the main.js file

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 /*
-Will serve as the server.
+Will serve as the server. Gateway Intents allow the bot to receive data based on what the bot needs to function.
+
+GatewayIntentBits Docs: https://discordjs.guide/popular-topics/intents.html#enabling-intents
+
 */
 
 const { Client, GatewayIntentBits } = require('discord.js');
+
+// Set a Client class to variable 'client'
 
 const client = new Client({
 	intents: [
